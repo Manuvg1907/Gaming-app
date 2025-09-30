c03aee339c70445e23eef672857a5e079c2fb582
Manuvg1907 <manuvgcs26@gmail.com>
2025-10-01 00:38:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,32 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+// Import screens
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import GameScreen from "./src/screens/GameScreen";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: "#4a90e2" },
+          tabBarLabelStyle: { fontSize: 14, fontWeight: "bold", color: "#fff" },
+          tabBarIndicatorStyle: { backgroundColor: "#fff" },
+          swipeEnabled: true,
+        }}
+      >
+        <Tab.Screen name="Login" component={LoginScreen} />
+        <Tab.Screen name="Register" component={RegisterScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Game" component={GameScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
