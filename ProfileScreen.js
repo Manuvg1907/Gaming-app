1541751a8c8b5bec80fe9e8b4bafa68aa80f1dbc
Manuvg1907 <manuvgcs26@gmail.com>
2025-10-02 16:41:11 +0530

    Update ProfileScreen.js

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,1 +1,18 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Screen</Text>
+      <Button title="Go to Game" onPress={() => navigation.navigate("Game")} />
+      <Button title="Logout" onPress={() => navigation.navigate("Login")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 24, fontWeight: "bold", marginBottom: 20 },
+});
 
