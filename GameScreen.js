460380a262bd5f65a97b27e267e3038de4beac1e
Manuvg1907 <manuvgcs26@gmail.com>
2025-10-03 17:18:16 +0530

    Update GameScreen.js

--- GameScreen.js
+++ GameScreen.js
@@ -1,1 +1,17 @@
 
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function GameScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🎲 Game Screen 🎲</Text>
+      <Button title="Back to Profile" onPress={() => navigation.navigate("Profile")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 24, fontWeight: "bold", marginBottom: 20 },
+});
