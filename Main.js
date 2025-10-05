65a270ce84d39435e71d96b28c1e9cb1743786e1
Manuvg1907 <manuvgcs26@gmail.com>
2025-10-05 23:43:40 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,10 @@
+// client/src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { GameProvider } from "./context/GameContext";
+import { SocketProvider } from "./context/SocketContext";
+
 
